61bba0e51db79e7230a8d8ee77089cdae9406f22
Lewin <lewinelst@gmail.com>
2023-04-16 12:57:47 +0100

    project upload

---
+++ ray.h
@@ -0,0 +1,29 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+//Ray is just a straight line equation orig is the origin vector and direction is the direction vector
+class ray { 
+	public: 
+		ray() {}
+		ray(const point3& origin, const vec3& direction, double time = 0.0)
+			: orig(origin), dir(direction), tm(time)
+		{}
+
+		point3 origin() const { return orig; }
+		vec3 direction() const { return dir; }
+		double time() const { return tm; }
+
+		point3 at(double t) const {
+			return orig + t * dir; //Returns a straight line equation
+		}
+
+	public:
+		point3 orig;
+		vec3 dir;
+		double tm;
+
+
+};
+#endif
